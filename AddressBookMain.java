8f7883ee132fe04df4928029743baf34b4787eda
Rajan-rkg9 <amirajangupta@gmail.com>
2020-09-29 18:56:15 +0530

    [Rajan-rkg9]Editing the Contact Details of a person added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -15,6 +15,39 @@ public class AddressBookMain {
 	{
 		contactList.add(obj);
 	}
+	public void editContact()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Do you want to edit contact details (Y/N-y/n): ");
+		char ch=sc.nextLine().charAt(0);
+		if(ch=='Y' || ch=='y')
+		{
+			System.out.println("Enter New Details: ");
+			System.out.println("Enter First Name: ");
+			String fn=sc.nextLine();
+			System.out.println("Enter Last Name: ");
+			String ln=sc.nextLine();
+			System.out.println("Enter Address: ");
+			String add=sc.nextLine();
+			System.out.println("Enter City: ");
+			String c=sc.nextLine();
+			System.out.println("Enter State: ");
+			String s=sc.nextLine();
+			System.out.println("Enter Zip Code: ");
+			String zc=sc.nextLine();
+			System.out.println("Enter Phone Number: ");
+			String pn=sc.nextLine();
+			System.out.println("Enter EmailId: ");
+			String ei=sc.nextLine();
+			Contacts contactObj = new Contacts(fn,ln,ei,c,add,zc,s,pn);
+			new AddressBookMain().addContact(contactObj);
+			System.out.println(contactObj);
+		}
+		else if(ch=='N' || ch=='n')
+			System.out.println("User does not want to edit details.");
+		else
+			System.out.println("Incorrect Input");
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Contact Details:");
@@ -38,5 +71,7 @@ public class AddressBookMain {
 	    AddressBookMain ob = new AddressBookMain();
 		ob.addContact(contactObj);
 		System.out.println(contactObj);
-	}
+		ob.editContact();
 }
+}
+
